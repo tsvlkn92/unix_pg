d8bdaac44388f719ad3285a0ec97d744bddf6988
tsvlkn92 <50596365+tsvlkn92@users.noreply.github.com>
2020-05-11 19:50:41 +0300

    Assigment Finish

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,11 +7,25 @@ answer=$(ls -1 |grep "^."|wc -l)
 echo $answer
 }
 
+function check_int { 
+re='^[0-9]+$'  
+temp=$1
+while ! [[ $temp =~ $re ]]  
+  do 
+    echo "Please Enter ONLY NUMBER!"
+    read temp 
+    #read $1 
+  done
+  ug=$temp
+}
+
 echo -n "Guess! How many files in my current directory : "
 
 tf=$(totalfile)  #Total File -> tf
 
 read ug #User Guess
+
+check_int $ug
 
 while true;
     do
@@ -19,10 +33,12 @@ while true;
 		 then
 			echo -n "You couldn't Guess, You entered a SMALL number! :"
 			read ug
+			check_int $ug
 		elif [[ $ug -gt $tf ]]
 		 then
 			echo -n "You couldn't guess, You entered a BIG number! :"
 			read ug
+			check_int $ug
 		else
 			echo "WE HAVE A WINNER! ;-) "
 		break;
